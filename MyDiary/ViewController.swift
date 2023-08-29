ea569b6314bbeb664d2fa60828ad7a5bd7f77a22
kdw <doowone8650@gmail.com>
2023-08-29 18:24:52 +0900

    Initial Commit

---
+++ MyDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDiary
+//
+//  Created by 김두원 on 2023/08/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
