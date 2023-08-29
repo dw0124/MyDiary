397328968e3cb3d6e8162958baa4b3330a7651da
kdw <doowone8650@gmail.com>
2023-08-29 20:06:04 +0900

    네이버 지도 추가

--- MyDiary/ViewController.swift
+++ MyDiary/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import NMapsMap
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let mapView = NMFMapView(frame: view.frame)
+        //let mapView = NMFNaverMapView(frame: view.frame)
+        view.addSubview(mapView)
+
+        let locationOverlay = mapView.locationOverlay
+        locationOverlay.hidden = true
+        
+        mapView.touchDelegate = self
     }
 
 
 }
 
+extension ViewController: NMFMapViewTouchDelegate {
+    func mapView(_ mapView: NMFMapView, didTapMap latlng: NMGLatLng, point: CGPoint) {
+        print("\(latlng.lat), \(latlng.lng)")
+    }
+}
