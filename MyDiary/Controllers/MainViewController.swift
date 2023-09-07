abe18b5e151e6ec690c10642b0d45f20129a2c0e
dw0124 <81311229+dw0124@users.noreply.github.com>
2023-09-08 00:40:55 +0900

    작업해야할 VC 생성

--- MyDiary/Controllers/MainViewController.swift
+++ MyDiary/Controllers/MainViewController.swift
@@ -5,4 +5,14 @@
 //  Created by 김두원 on 2023/09/03.
 //
 
+import UIKit
 import Foundation
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
---
+++ MyDiary/Controllers/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  MyDiary
+//
+//  Created by 김두원 on 2023/09/04.
+//
+
+import UIKit
+import Foundation
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+    
+}
